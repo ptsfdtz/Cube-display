f380c862db43fe7aabb4db88437a136ab3fec640
秃子喵 <pitousanfadetuzi@gmail.com>
2024-09-06 11:40:25 +0800

    first commit

---
+++ src/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".push-me-button");
+  button.addEventListener("click", function () {
+    alert("Button was clicked!");
+  });
+});
